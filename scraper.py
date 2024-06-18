d07b6f2fbe6ba0d6c4327d14107388b6a4a92770
EmmaEnkhbold <107696520+EmmaEnkhbold@users.noreply.github.com>
2024-06-18 17:35:51 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,1 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
 
+#CNN Articles Website
+URL = "https://www.cnn.com/articles"
+
+#retrieve the HTML content
+request = requests.get(URL)
+pageContent = request.content
+
+#parse with BeautifulSoup object
+soup = BeautifulSoup(pageContent, 'html.parser')
+
+#find all headings on the page
+headings = soup.find_all("h3")
+
+#print the text of all headings
+for i in headings:
+    print(i.text)
